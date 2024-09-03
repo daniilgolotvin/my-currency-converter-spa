0b513148b8f86dfb47c38ac13535c16530c781d3
DANIIL GOLOTVIN <daniilgolotvin@yandex.ru>
2024-09-03 17:27:50 +0300

    fixed object duplication error

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/my-currency-converter-spa/'
-    : '/'
-}
 
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/my-currency-converter-spa/'  // Замените на ваше имя репозитория
+    : '/'
+})
